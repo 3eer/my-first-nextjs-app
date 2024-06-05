bcf62325139ac830da66b9ba00d35919177f65fd
3eer <t4keru.rneuo@gmail.com>
2024-06-05 14:50:32 +0900

    feature dashboard page

--- prisma/migrations/20240605045448_init/migration.sql
+++ prisma/migrations/20240605045448_init/migration.sql
@@ -37,5 +37,21 @@ CREATE TABLE `invoices` (
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- CreateTable
+CREATE TABLE `revenues` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `user_id` VARCHAR(191) NOT NULL,
+    `month` VARCHAR(6) NOT NULL,
+    `revenue` INTEGER NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `revenues_user_id_month_key`(`user_id`, `month`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 -- AddForeignKey
 ALTER TABLE `invoices` ADD CONSTRAINT `invoices_customer_id_fkey` FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `revenues` ADD CONSTRAINT `revenues_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
