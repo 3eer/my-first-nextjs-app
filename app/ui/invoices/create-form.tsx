96b024e321f4537d917701c4616f0be8a868d93e
3eer <t4keru.rneuo@gmail.com>
2024-06-20 08:15:56 +0900

    fix: indent

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -72,13 +72,13 @@ export default function Form({ customers }: { customers: Customer[] }) {
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
             {state.errors?.amount && (
-            <div id="customer-error" aria-live="polite" aria-atomic="true">
-              <ErrorMessages
-                errors={state.errors?.amount}
-                className="mt-2 text-sm text-red-500"
-              />
-            </div>
-          )}
+              <div id="customer-error" aria-live="polite" aria-atomic="true">
+                <ErrorMessages
+                  errors={state.errors?.amount}
+                  className="mt-2 text-sm text-red-500"
+                />
+              </div>
+            )}
           </div>
         </div>
 
@@ -125,13 +125,13 @@ export default function Form({ customers }: { customers: Customer[] }) {
           </div>
         </fieldset>
         {state.errors?.status && (
-            <div id="customer-error" aria-live="polite" aria-atomic="true">
-              <ErrorMessages
-                errors={state.errors?.status}
-                className="mt-2 text-sm text-red-500"
-              />
-            </div>
-          )}
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            <ErrorMessages
+              errors={state.errors?.status}
+              className="mt-2 text-sm text-red-500"
+            />
+          </div>
+        )}
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
