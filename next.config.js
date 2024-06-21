29184b67085a349c57e05466881b0d8afe56320e
3eer <t4keru.rneuo@gmail.com>
2024-06-21 10:56:27 +0900

    ad security headers

--- next.config.js
+++ next.config.js
@@ -1,10 +1,45 @@
 /** @type {import('next').NextConfig} */
 require('dotenv').config();
 const nextConfig = {
+  reactStrictMode: true,
   env: {
     ORIGIN: process.env.ORIGIN,
     AUTH_SECRET: process.env.AUTH_SECRET,
   },
+  headers: [
+    {
+      key: 'X-DNS-Prefetch-Control',
+      value: 'on',
+    },
+    {
+      key: 'Strict-Transport-Security',
+      value: 'max-age=63072000; includeSubDomains; preload',
+    },
+    {
+      key: 'X-DNS-Prefetch-Control',
+      value: 'on'
+    },
+    {
+      key: 'Strict-Transport-Security',
+      value: 'max-age=63072000; includeSubDomains; preload'
+    },
+    {
+      key: 'X-Frame-Options',
+      value: 'DENY'
+    },
+    {
+      key: 'X-Content-Type-Options',
+      value: 'nosniff'
+    },
+    {
+      key: 'Referrer-Policy',
+      value: 'origin-when-cross-origin'
+    },
+    {
+      key: 'Content-Security-Policy',
+      value: "default-src 'self'; img-src *;"
+    },
+  ],
 };
 
 module.exports = nextConfig;
