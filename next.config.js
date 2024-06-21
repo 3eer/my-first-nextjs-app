045f0ad11d8edd7b1a3f07ec2afb5b920c8bdc40
3eer <t4keru.rneuo@gmail.com>
2024-06-21 13:21:42 +0900

    change images path

--- next.config.js
+++ next.config.js
@@ -5,48 +5,77 @@ const nextConfig = {
   env: {
     ORIGIN: process.env.ORIGIN,
     AUTH_SECRET: process.env.AUTH_SECRET,
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    IMAGE_HOST: process.env.IMAGE_HOST,
   },
   images: {
-    domains: [
-      'localhost',
-      process.env.IMAGE_HOST ? process.env.IMAGE_HOST : '',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.IMAGE_HOST,
+        port: '',
+        pathname: '/**/*',
+      },
     ],
   },
-  headers: [
-    {
-      key: 'X-DNS-Prefetch-Control',
-      value: 'on',
-    },
-    {
-      key: 'Strict-Transport-Security',
-      value: 'max-age=63072000; includeSubDomains; preload',
-    },
-    {
-      key: 'X-DNS-Prefetch-Control',
-      value: 'on'
-    },
-    {
-      key: 'Strict-Transport-Security',
-      value: 'max-age=63072000; includeSubDomains; preload'
-    },
-    {
-      key: 'X-Frame-Options',
-      value: 'DENY'
-    },
-    {
-      key: 'X-Content-Type-Options',
-      value: 'nosniff'
-    },
-    {
-      key: 'Referrer-Policy',
-      value: 'origin-when-cross-origin'
-    },
-    {
-      key: 'Content-Security-Policy',
-      value: "default-src 'self'; img-src *;"
-    },
-  ],
+  async headers() {
+    return [
+      {
+        source: '/api/(.*)',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json'
+          },
+        ],
+      },
+      {
+        source: '/images/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          },
+        ],
+      },
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+          },
+          // {
+          //   key: 'Content-Security-Policy',
+          //   value: "default-src 'self'; img-src *;"
+          // },
+        ]
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
